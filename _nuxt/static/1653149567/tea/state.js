6f5e5063c6495a95e4f89f83d7e721ed65811b2c
tsuyu0718 <tsuyu0718@users.noreply.github.com>
2022-05-21 16:12:50 +0000

    deploy: f455b023d02061aea13557ab20ac226c0276d44e

---
+++ _nuxt/static/1653149567/tea/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fzatta-tools\u002F_nuxt\u002Fstatic\u002F1653149567",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Ftea",config:{_app:{basePath:"\u002Fzatta-tools\u002F",assetsPath:"\u002Fzatta-tools\u002F_nuxt\u002F",cdnURL:a}}}}(null));
