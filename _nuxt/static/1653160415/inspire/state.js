de95b3d212cf66c35c34ecdbe7a796da0c721bc3
tsuyu0718 <tsuyu0718@users.noreply.github.com>
2022-05-22 15:51:49 +0000

    deploy: 083e8d2f329a562485054253497c224e549a0548

--- _nuxt/static/1653160415/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fzatta-tools\u002F_nuxt\u002Fstatic\u002F1653160415",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fzatta-tools\u002F",assetsPath:"\u002Fzatta-tools\u002F_nuxt\u002F",cdnURL:a}}}}(null));
