d7867c6aa31239ac5aafd53f89bf4290924185aa
tsuyu0718 <tsuyu0718@users.noreply.github.com>
2022-05-29 15:06:00 +0000

    deploy: 66740590b8a59a155bb43577e553f51d35f04518

--- _nuxt/static/1653318723/chat/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fzatta-tools\u002F_nuxt\u002Fstatic\u002F1653318723",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fchat",config:{_app:{basePath:"\u002Fzatta-tools\u002F",assetsPath:"\u002Fzatta-tools\u002F_nuxt\u002F",cdnURL:a}}}}(null));
